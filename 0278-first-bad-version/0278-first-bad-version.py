ba7363a0fd41538a05ff5d770f45bad9f388100b
AMIR SHAHZAD HASHMI <150381267+amirshahzadhashmi7145@users.noreply.github.com>
2026-08-31 03:53:27 +0500

    Time: 11 ms (85.82%), Space: 12.4 MB (14.29%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -0,0 +1,25 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        low, high = 1, n
+   
+        while low < high:
+         mid = (low+high) // 2
+         if isBadVersion(mid):
+          high = mid
+         else:
+          low = mid+1
+         
+        return low
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
