06c2b92ac2e887f3c86e43f979932feb3cdf2462
AMIR SHAHZAD HASHMI <150381267+amirshahzadhashmi7145@users.noreply.github.com>
2026-09-19 05:09:00 +0500

    Time: 19 ms (76.5%), Space: 16.1 MB (56.1%) - LeetHub - Time: O(N * K log K), Space: O(N * K)

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        groups = {}
+        
+        for s in strs:
+         card = ''.join(sorted(s))
+         if card not in groups:
+          groups[card] = []
+         groups[card].append(s)
+        
+        return list(groups.values())
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
